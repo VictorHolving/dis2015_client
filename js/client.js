114218db3f12ce7510d5838c627bcd6f29f475ee
VictorHolving <vholving@gmail.com>
2015-11-13 22:44:36 +0100

    Main Menu JS

---
+++ js/client.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Victor on 13/11/15.
+ */
+$(document).ready(function(){
+
+    $("#SignOut").click(function(){
+
+        window.location="http://localhost:63342/dis2015_client/html/MainMenu.html";
+    });
+
+
+});
