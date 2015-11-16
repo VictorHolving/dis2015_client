419e2874d8818647a569110254438522e85f9c3a
VictorHolving <vholving@gmail.com>
2015-11-16 22:13:46 +0100

    ShowGames function created but not working

--- js/client.js
+++ js/client.js
@@ -2,26 +2,32 @@ $(document).ready(function() {
 
     <!--Login function-->
     $("#LoginButton").click(function () {
+        <!--Create variable-->
         var signinInfo = {
             username : $("#username").val(),
             password : $("#password").val()
         };
-        <!--Create variable with parameters-->
+        <!--parameters-->
         var settings = {
             "async" : true,
-            <!--fra postman-->
             "url": "http://localhost:8888/api/login/",
             "method": "POST",
             "processData": false,
             "data" : JSON.stringify(signinInfo)
-    };  <!--Refresh site with variable-->
+    };  <!--Refresh site with variable parameters-->
         $.ajax(settings).done(function (response) {
             console.log(response);
             window.location.replace("MainMenu.html");
         });
     });
 
+    <!--show games function-->
+    $("#ShowGamesButton").click(function () {
+        $.ajax().done(function () {
+            console.log.url("localhost:888/api/games");
+        });
 
+    });
 
 
 
